3038178560405e2beace7394dd240acc51c99eed
Muhammad Talha <talha18513@gmail.com>
2025-05-12 14:27:13 +0500

    notification add

--- Docs/javascript/navbar.js
+++ Docs/javascript/navbar.js
@@ -1,39 +1,41 @@
 const navbar = document.getElementById("navbar")
 
 navbar.innerHTML = `
-    <nav class="fixed top-0 left-0 z-40 w-full bg-[#1F4B44]">
-        <div class="max-w-7xl mx-auto flex justify-between items-center w-full md:p-5 p-3 md:h-28 h-24">
-            <a data-aos="fade-right" href="./index.html" class="flex justify-center lg:justify-start">
-                <img src="./Docs/Assets/images/logo-white.png" class="md:h-24 h-16" alt="logo">
-            </a>
-            <div class="md:flex items-center gap-10 hidden">
-                <ul data-aos="fade-left" class="md:flex hidden text-[#fff] items-center gap-10">
-                    <li><a href="#Überuns"
-                            class="hover:text-[#d4e2dd] tracking-wide text-lg transition-all duration-500">Über uns </a>
-                    </li>
-                    <li><a href="#UnsereDienstleistungen"
-                            class="hover:text-[#d4e2dd] tracking-wide text-lg transition-all duration-500">Service
-                        </a>
-                    </li>
-                    <li><a href="#Kontaktieren"
-                            class="hover:text-[#d4e2dd] tracking-wide text-lg transition-all duration-500">Kontakt
-                        </a>
-                    </li>
-                    <li><a href="mailto:hello@nestcare.ch"
-                            class="hover:text-[#d4e2dd] flex items-center gap-2 tracking-wide text-lg transition-all duration-500"><i
-                                class="fa-solid fa-envelope text-[#fff]"></i>hello@nestcare.ch
-                        </a>
-                    </li>
-                    <li><a href="tel:+41783373343"
-                            class="hover:text-[#d4e2dd] flex items-center gap-2 tracking-wide text-lg transition-all duration-500"><i
-                                class="fa-brands text-xl fa-whatsapp text-[#fff]"></i> +41783373343
-                        </a>
-                    </li>
-                </ul>
+        <nav class="fixed top-0 left-0 z-40 w-full bg-[#1F4B44]">
+            <div class="max-w-7xl mx-auto flex justify-between items-center w-full md:p-5 p-3 md:h-28 h-24">
+                <a data-aos="fade-right" href="./index.html" class="flex justify-center lg:justify-start">
+                    <img src="./Docs/Assets/images/logo-white.png" class="md:h-24 h-16" alt="logo">
+                </a>
+                <div data-aos="fade-left" class="items-center flex md:gap-10 gap-5">
+                    <ul  class="flex md:flex-row flex-col text-[#fff] items-center md:gap-10">
+                        <li><a href="#Überuns"
+                                class="hover:text-[#d4e2dd] md:block hidden tracking-wide text-lg transition-all duration-500">Über uns
+                            </a>
+                        </li>
+                        <li><a href="#UnsereDienstleistungen"
+                                class="hover:text-[#d4e2dd] md:block hidden tracking-wide text-lg transition-all duration-500">Service
+                            </a>
+                        </li>
+                        <li><a href="#Kontaktieren"
+                                class="hover:text-[#d4e2dd] md:block hidden tracking-wide text-lg transition-all duration-500">Kontakt
+                            </a>
+                        </li>
+                        <li><a href="mailto:hello@nestcare.ch"
+                                class="hover:text-[#d4e2dd] flex md:text-[16px] text-sm items-center gap-2 tracking-wide text-lg transition-all duration-500"><i
+                                    class="fa-solid fa-envelope text-[#fff]"></i>hello@nestcare.ch
+                            </a>
+                        </li>
+                        <li><a href="tel:+41783373343"
+                                class="hover:text-[#d4e2dd] flex md:text-[16px] text-sm items-center gap-2 tracking-wide text-lg transition-all duration-500"><i
+                                    class="fa-brands text-xl fa-whatsapp text-[#fff]"></i> +41783373343
+                            </a>
+                        </li>
+                    </ul>
+                    <i id="mobileMenuBtn"
+                    class="fa-solid text-[#d4e2de] fa-bars md:!hidden text-2xl cursor-pointer mr-2"></i>
+                </div>
             </div>
-            <i id="mobileMenuBtn" class="fa-solid text-[#d4e2de] fa-bars md:!hidden text-2xl cursor-pointer mr-2"></i>
-        </div>
-    </nav>
+        </nav>
 
     <div id="mobileMenu"
         class="fixed left-0 top-0 bg-[#d4e2de] w-0 overflow-hidden h-screen z-50 transition-all duration-500">
